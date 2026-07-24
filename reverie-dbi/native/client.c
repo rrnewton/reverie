d1e050779ec9c88ba0dc0aede64e0c326838a418
Ryan Newton <rrnewton@gmail.com>
2026-07-24 09:19:43 -0700

    Preserve DBI image generation after failed exec

--- reverie-dbi/native/client.c
+++ reverie-dbi/native/client.c
@@ -746,8 +746,6 @@ static void post_syscall(void *drcontext, int sysnum) {
                                                     thread_state_index);
     DR_ASSERT(counters != NULL);
     reverie_dbi_runtime_exec_failed(counters, (int32_t)dr_get_process_id());
-    atomic_store_explicit(&image_generation, reverie_dbi_runtime_image_init(),
-                          memory_order_release);
     if (!dr_create_client_thread(runtime_background_init, NULL))
       DR_ASSERT(false);
     return;
